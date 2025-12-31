f5dbead6708b88eebc69a77a6cf1a8bd7f2eb28b
Prithujit Ghosh <prithujitghosh0620@gmail.com>
2025-12-31 12:41:43 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,4 @@
+music={
+    "cold":"https://www.youtube.com/watch?v=Vhd6Kc4TZls&list=PLzCxunOM5WFJ7sbHi_9Zwq2xOwtkYeZlx&index=10",
+    "ether":"https://www.youtube.com/watch?v=r6En29azNBA&list=PLzCxunOM5WFJ7sbHi_9Zwq2xOwtkYeZlx&index=11"
+}
